4cf65d95e094e7111b5aff379570d80b926ce1fe
jmc359 <joe.connolly@yale.edu>
2018-07-30 09:39:35 -0400

    updated data recording script

--- p3dx_2dnav/scripts/data.py
+++ p3dx_2dnav/scripts/data.py
@@ -8,23 +8,26 @@ import rospkg
 import time
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, Pose, PoseArray
 from podi_move_base_msgs.msg import PodiMoveBaseActionGoal
-from nav_msgs.msg import Path
+from nav_msgs.msg import Path, Odometry
 from std_msgs.msg import Float64
 
 rospack = rospkg.RosPack()
 directory = rospack.get_path("p3dx_2dnav")
 timestr = time.strftime("%Y%m%d-%H%M%S")
-rname = timestr + "podi.json"
+rname1 = timestr + "rviz.json"
+rname2 = timestr + "gazebo.json"
 pname = timestr + "person.json"
 oname = timestr + "obstacle.json"
 gname = timestr + "goal.json"
 planner_name = timestr + "plan.json"
-rpath = directory + '/simulations/' + rname
+rpath1 = directory + '/simulations/' + rname1
+rpath2 = directory + '/simulations/' + rname2
 ppath = directory + '/simulations/' + pname
 opath = directory + '/simulations/' + oname
 gpath = directory + '/simulations/' + gname
 planner_path = directory + '/simulations/' + planner_name
-podi = {"podi": []}
+rviz = {"rviz": []}
+gazebo = {"gazebo": []}
 obstacle = {"obstacle": []}
 goal = {"goal": [], "planner": ""}
 person = {"person": []}
@@ -32,68 +35,68 @@ planner = {"planner": []}
 
 class Record(object):
     def __init__(self):
-        self._rpose_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self._rpose_cb, queue_size=1)
-        self._ppose_sub = rospy.Subscriber('/coupling_model_node/human_position', PoseStamped, self._ppose_cb, queue_size=1)
+        # robot and human poses
+        self._rviz_sub = rospy.Subscriber('/convert_poses/pose_converted', PoseStamped, self._rviz_cb, queue_size=1)
+        self._gazebo_sub = rospy.Subscriber('/p3dx/base_pose_ground_truth', Odometry, self._gazebo_cb, queue_size=1)
+        self._person_sub = rospy.Subscriber('/coupling_model_node/human_position', PoseStamped, self._person_cb, queue_size=1)
+        # pose msg params
+        self._last_rviz = None
+        self._last_gazebo = None
+        self._last_person = None
+        
+        # obstacle distance from human and obstalce msg param
         self._obstacle_sub = rospy.Subscriber('/obstacle_dist', Float64, self._obstacle_cb, queue_size=1)
-        #self._goal_sub = rospy.Subscriber('/podi_move_base/goal', PodiMoveBaseActionGoal, self._goal_cb, queue_size=1)
-        #self._goal_sub = rospy.Subscriber('/podi_move_base/current_goals', PoseArray, self._goal_cb, queue_size=1)
+        self._last_obstacle = None
+        
+        # global path and path msg param
         self._planner_sub = rospy.Subscriber('/podi_move_base/GlobalPlanner/robot_global_plan', Path, self._planner_cb, queue_size=1)
+        self._last_path = None
+        
+        # goal and goal msg params
         self._r_endgoal_sub = rospy.Subscriber("/podi_move_base/GlobalPlanner/robot_traj_goal", PoseArray, self._rend_cb, queue_size=1)
         self._c_endgoal_sub = rospy.Subscriber("/podi_move_base/GlobalPlanner/human_traj_goal", PoseArray, self._cend_cb, queue_size=1)
-        self._pose_pub = rospy.Publisher('/robot_position', PoseStamped, queue_size=2)
-        self._rlast_pose = None
-        self._plast_pose = None
-        self._last_obstacle = None
-        #self._last_goal = None
         self._last_rgoal = None
         self._last_cgoal = None
-        self._last_path = None
+        ###self._goal_sub = rospy.Subscriber('/podi_move_base/current_goals', PoseArray, self._goal_cb, queue_size=1)
+        ###self._goal_sub = rospy.Subscriber('/podi_move_base/goal', PodiMoveBaseActionGoal, self._goal_cb, queue_size=1)
+        ###self._last_goal = None
 
-    def _rpose_cb(self, msg):
-    	if msg != None:
-	        new_msg = PoseStamped()
-	        new_msg.header = msg.header
-	        new_msg.pose.position = msg.pose.pose.position
-	        new_msg.pose.orientation = msg.pose.pose.orientation
-	        self._pose_pub.publish(new_msg)
+    def _rviz_cb(self, msg):
+    	#if msg != None:
+	    #    new_msg = PoseStamped()
+	    #    new_msg.header = msg.header
+	    #    new_msg.pose.position = msg.pose.pose.position
+	    #    new_msg.pose.orientation = msg.pose.pose.orientation
+	    #    self._pose_pub.publish(new_msg)
 
-        if msg != self._rlast_pose:
+        if msg != self._last_rviz:
             y = yaml.load(str(msg))
-            podi["podi"].append(y)
-        self._rlast_pose = msg      
+            rviz["rviz"].append(y)
+        self._last_rviz = msg  
+        
+    def _gazebo_cb(self, msg):
+        if msg != self._last_gazebo:
+            y = yaml.load(str(msg))
+            gazebo["gazebo"].append(y)
+        self._last_gazebo = msg          
 
-    def _ppose_cb(self, msg):
-        if self._plast_pose != None and (msg.pose.position != self._plast_pose.pose.position or msg.pose.orientation != self._plast_pose.pose.orientation):
+    def _person_cb(self, msg):
+        if self._last_person != None and (msg.pose.position != self._last_person.pose.position or msg.pose.orientation != self._last_person.pose.orientation):
             y = yaml.load(str(msg))
             person["person"].append(y)
-        self._plast_pose = msg
+        self._last_person = msg
 	
     def _obstacle_cb(self, msg):
         if msg != self._last_obstacle and self._last_obstacle != None:
             y = yaml.load(str(msg))
             obstacle["obstacle"].append(y)
         self._last_obstacle = msg
-
-    '''def _goal_cb(self, msg):
-        if self._last_goal == None:
+	    
+    def _planner_cb(self, msg):
+        if msg != self._last_path:
             y = yaml.load(str(msg))
-            x = rospy.get_param("/podi_move_base/base_global_planner")
-            if x == "podi_robot_only_planner/GlobalPlanner":
-	            goal["planner"] = "robot_only"
-            else:
-                goal["planner"] = "coupled"
-                goal["goal"].append(y)
-	    self._last_goal = msg'''
-    def _goal_cb(self, msg):
-        if self._last_goal != None:
-            y = yaml.load(str(msg))
-            x = rospy.get_param("/podi_move_base/base_global_planner")
-            if x == "podi_robot_only_planner/GlobalPlanner":
-                goal["planner"] = "robot_only"
-            else:
-                goal["planner"] = "coupled"
-            goal["goal"].append(y)
-        self._last_goal = msg
+            planner["planner"].append(y)
+        self._last_plan = msg
                 
     def _rend_cb(self, msg):
         if self._last_rgoal != None:
@@ -110,18 +113,26 @@ class Record(object):
             goal["planner"] = "coupled"
             goal["goal"].append(y)
         self._last_cgoal = msg
-
-    def _planner_cb(self, msg):
-        if msg != self._last_path:
-            y = yaml.load(str(msg))
-            planner["planner"].append(y)
-        self._last_plan = msg
 	
+    #def _goal_cb(self, msg):
+    #    if self._last_goal != None:
+    #        y = yaml.load(str(msg))
+    #        x = rospy.get_param("/podi_move_base/base_global_planner")
+    #        if x == "podi_robot_only_planner/GlobalPlanner":
+    #            goal["planner"] = "robot_only"
+    #        else:
+    #            goal["planner"] = "coupled"
+    #        goal["goal"].append(y)
+    #    self._last_goal = msg
 
 def handle():
-    if len(podi["podi"]) > 1:
-        with open(rpath, "w+") as f:
-            f.write(json.dumps(podi, indent=4))
+    if len(rviz["rviz"]) > 1:
+        with open(rpath1, "w+") as f:
+            f.write(json.dumps(rviz, indent=4))
+    
+    if len(gazebo["gazebo"]) > 1:
+        with open(rpath2, "w+") as f:
+            f.write(json.dumps(gazebo, indent=4))
     
     if len(person["person"]) > 1:
         with open(ppath, "w+") as f:
@@ -131,11 +142,11 @@ def handle():
         with open(opath, "w+") as f:
             f.write(json.dumps(obstacle, indent=4))
 
-    if len(podi["podi"]) > 1:
+    if len(rviz["rviz"]) > 1:
         with open(gpath, "w+") as f:
             f.write(json.dumps(goal, indent=4))
 
-    if len(podi["podi"]) > 1:
+    if len(rviz["rviz"]) > 1:
         with open(planner_path, "w+") as f:
             f.write(json.dumps(planner, indent=4))
 
