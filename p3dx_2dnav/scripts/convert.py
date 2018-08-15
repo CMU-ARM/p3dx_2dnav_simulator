acde9602f0ff89d7456f779ec21ec2ed66f3da79
jmc359 <jconnolly171@gmail.com>
2018-08-15 17:25:21 -0400

    handle length update

--- p3dx_2dnav/scripts/convert.py
+++ p3dx_2dnav/scripts/convert.py
@@ -1,20 +1,20 @@
-import json 
+import json
 from math import sin, cos, radians, pi
 
 class Convert():
-    def robot_to_human(self, x0, y0, theta, r=0.79898):
+    def robot_to_human(self, x0, y0, theta, r=0.629):
         point = []
         theta = (theta + 180) * pi / 180
-        x = x0 + r * cos(theta) 
+        x = x0 + r * cos(theta)
         y = y0 + r * sin(theta)
         point.append(x)
         point.append(y)
         return point
 
-    def human_to_robot(self, x0, y0, theta, r=0.79898):
+    def human_to_robot(self, x0, y0, theta, r=0.629):
         point = []
         theta = theta * pi / 180
-        x = x0 + r * cos(theta) 
+        x = x0 + r * cos(theta)
         y = y0 + r * sin(theta)
         point.append(x)
         point.append(y)
